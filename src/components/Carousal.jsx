b7ad715aa44b65faa0a03a2d112f90686a8d271e
abay777 <abaysankar777@gmailcom>
2024-06-17 09:29:47 +0530

    bug fixed

--- src/components/Carousal.jsx
+++ src/components/Carousal.jsx
@@ -10,16 +10,48 @@ const Carousels = () => {
          showIndicators={false}>
                     <div className=" w-full h-[500px] ">
                         <img src="/carousal1.webp"  />
+                        <p className="legend mb-5 opacity-100 text-white ">
+                            <div className="ml-12 text-2xl opacity-100 text-white hover:text-white uppercase">
+                                bespoken
+                            </div>
+                            <div className=" text-xl uppercase border-2 p-0 w-fit ml-28 py-1 px-2 mt-1 ">
+                                cusion cover
+                            </div>
+                        </p>
                     </div>
                     <div className="w-full">
                         <img src="/carousal4.webp"  />
+                        <p className="legend mb-5 opacity-100 text-white ">
+                            <div className="ml-12 text-2xl opacity-100 text-white hover:text-white uppercase">
+                                bespoken
+                            </div>
+                            <div className=" text-xl uppercase border-2 p-0 w-fit ml-28 py-1 px-2 mt-1 ">
+                                cusion cover
+                            </div>
+                        </p>
                        
                     </div>
                     <div className="w-full">
                         <img src="/carousal3.webp"   />
+                        <p className="legend mb-5 opacity-100 text-white ">
+                            <div className="ml-12 text-2xl opacity-100 text-white hover:text-white uppercase">
+                                bespoken
+                            </div>
+                            <div className=" text-xl uppercase border-2 p-0 w-fit ml-28 py-1 px-2 mt-1 ">
+                                cusion cover
+                            </div>
+                        </p>
                     </div>
                     <div className="w-full"> 
                         <img src="/carousal4.webp"   />
+                        <p className="legend mb-5 opacity-100 text-white ">
+                            <div className="ml-12 text-2xl opacity-100 text-white hover:text-white uppercase">
+                                bespoken
+                            </div>
+                            <div className=" text-xl uppercase border-2 p-0 w-fit ml-28 py-1 px-2 mt-1 ">
+                                cusion cover
+                            </div>
+                        </p>
                     </div>
                 </Carousel>
     </main>
